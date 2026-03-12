0a9ab758070e8cb70bf79e2074f7a3f15ec8b6f5
Michael de Vries <vriesdemichael@gmail.com>
2026-03-12 16:28:31 +0100

    update zeepkist

---
+++ presentations/zeepkist-agentic-coding/components/CoverSlide.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cover-slide">
+    <img class="cover-bg-art" src="/images/cover-bg.jpg" alt="Achtergrondillustratie" />
+    <div class="cover-left-stack">
+      <div class="cover-block-main">
+        <div class="cover-content">
+          <h1 class="cover-title">Agentic coding in de praktijk</h1>
+          <p class="cover-sub">
+            Waarom nu?<br />
+            Hoe doe je het?<br />
+            Wat voor tools zijn er?<br />
+            Wat kan je leren van de zeik blogs.
+          </p>
+        </div>
+      </div>
+
+      <div class="cover-block-bottom">
+        <div class="cover-footer">
+          <div class="cover-date">24 maart 2026</div>
+          <div>Michaël de Vries</div>
+          <div>Zeepkist · Deel 1</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
